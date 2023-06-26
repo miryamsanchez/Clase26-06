9fa05134297e3d3c77ff53d22f69dcc97d504a51
nuevo user <user@gmail.com>
2023-06-26 14:27:26 +0200

    mas

---
+++ juego3raya.py
@@ -0,0 +1,19 @@
+class TresenRaya:
+    def __init__(self, tablero, casilla_vacia, ficha_equis, ficha_circulo) :
+        self.tablero = tablero
+        self.casilla_vacia = casilla_vacia
+        self.ficha_equis = ficha_equis
+        self.ficha_circulo = ficha_circulo
+
+    def colocarFicha(ficha,x,y):
+    
+
+
+class Juego:
+    def __init__(self) -> None:
+        self.TresenRaya
+        self.jugadores
+    
+    def main():
+        
+        
